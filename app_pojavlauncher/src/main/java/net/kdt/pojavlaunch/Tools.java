94902a98642f35b3f313fc80d9cf7bbe9d1bfe9b
Ninhoplayer6gg <filhofilho281@gmail.com>
2026-02-26 11:15:56 -0300

    Update Tools.java
    Signed-off-by: Ninhoplayer6gg <filhofilho281@gmail.com>

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -16,6 +16,7 @@ import android.app.ProgressDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
